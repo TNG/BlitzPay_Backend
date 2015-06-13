ace5fff8e01ca3a43e8b2ca0cd08eb6f1268fb4b
Franziska Hinkelmann <franziska.hinkelmann@gmail.com>
2015-06-13 15:02:35 +0200

    Save events to memory

--- routes/event.js
+++ routes/event.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/:id', function(req, res, next) {
-    var event = {
-        "eventId": req.params.id,
-        "name": "lunch",
-        "amount": 52,
-        "currency": "EUR",
-        "creator": {
-            "name": "Franzi",
-            "address": req.params.id
+router.get('/:id', function (req, res, next) {
+
+    var resp = "";
+    for (var i = 0; i < globalEvents.length; i++) {
+        var event = globalEvents[i];
+        var id = event.eventId;
+        resp = resp + id;
+
+        if (id == req.params.id) {
+            res.json(event);
+            return;
         }
-    };
-    res.json(event);
+    }
+    res.send("Could not find event wit Id " + req.params.id + " current events: " + resp);
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
     var event = req.body;
-
-    res.send("New event was created " + JSON.stringify(event));
+    event.eventId = globalEvents.length + 38;
+    globalEvents.push(event);
+    res.send("New event was created with id: " + event.eventId);
 });
 
 module.exports = router;
